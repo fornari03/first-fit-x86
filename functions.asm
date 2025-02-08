9ac164b7fd3df1a6558153cee0abbbb3004859ba
fornari03 <221017023@aluno.unb.br>
2025-02-08 15:50:30 -0300

    feat: implementa loop de subtração do acumulador

--- functions.asm
+++ functions.asm
@@ -1,12 +1,35 @@
+%define quant_blocks [ebp+8]
+%define program_size [ebp+12]
+
 section .data
+used_blocks db 0
 
 
 section .text
 global f1
 
-f1:     enter 0, 0
-    
-        mov eax, DWORD [ebp+8]
+f1:             enter 0, 0
 
-        leave
-        ret
+                push ecx
+                push ebx
+        
+                mov eax, DWORD program_size             ; eax = tamanho do programa
+                mov ecx, DWORD quant_blocks             ; ecx = quantidade de blocos
+                mov ebx, 5                              ; ebx = indice na pilha
+                ;mov ecx, 1
+
+repete: 
+                cmp eax, DWORD [ebp+ebx*4]              ; programa <= bloco
+                jle encerra_f1                          ; ? encerra_f1 : continua loop
+                sub eax, DWORD [ebp+ebx*4]              ; programa -= bloco
+
+                add ebx, 2
+                loop repete
+
+encerra_f1:
+                
+                pop ebx
+                pop ecx
+
+                leave
+                ret
