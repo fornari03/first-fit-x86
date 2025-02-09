11595cf7d90621690778be8f1b6937663241a387
fornari03 <221017023@aluno.unb.br>
2025-02-09 00:29:48 -0300

    feat: faz esboço da chamada de f2

--- functions.asm
+++ functions.asm
@@ -1,22 +1,20 @@
 %define quant_blocks [ebp+8]
 %define program_size [ebp+12]
+%define last_aloc [ebp-5]
 
 section .data
-used_blocks db 0
-
 
 section .text
 global f1
 
-f1:             enter 0, 0
-
+f1:             enter 5, 0
                 push ecx
                 push ebx
         
                 mov eax, DWORD program_size             ; eax = tamanho do programa
                 mov ecx, DWORD quant_blocks             ; ecx = quantidade de blocos
                 mov ebx, 5                              ; ebx = indice na pilha
-                ;mov ecx, 1
+                mov BYTE [ebp-1], 1                     ; flag pra dizer se coube
 
 repete: 
                 cmp eax, DWORD [ebp+ebx*4]              ; programa <= bloco
@@ -26,10 +24,53 @@ repete:
                 add ebx, 2
                 loop repete
 
+                ; se chegou aqui, o programa não coube nos blocos
+                mov BYTE [ebp-1], 0
+
 encerra_f1:
+                dec ebx
+                mov DWORD last_aloc, ebx
+                mov ebx, 5
+                ; eax = restante de espaço do programa a ser alocado
+                ; ebx = 5 (endereço inicial do 1° bloco)
+                ; last_aloc = indice na pilha pro endereço do ultimo bloco que aloca espaço [FIXO]
+                ; ecx -> não importa
+                movzx ecx, BYTE [ebp-1]
+                push ecx                                ; push flag de dizer se coube
+                mov ecx, DWORD quant_blocks
+                push ecx                                ; push quantidade de blocos
+
+                ; enquanto ebx <|<= last_aloc:
+                ;     mov ecx, DWORD [ebp+ebx*4]
+                ;     push ecx                          ; push endereço inicial
+                ;     inc ebx
+                ;     VERIFICAR VALOR DO EAX
+                ;     add ecx, DWORD [ebp+ebx*4]
+                ;     dec ecx 1
+                ;     push ecx                          ; push endereço final usado
+                ;     push ecx                          ; push ultimo endereço do bloco
+
+                ; enquanto ebx <|<= quant_blocks*2
+                ;     mov ecx, DWORD [ebp+ebx*4]
+                ;     push ecx                          ; push endereço inicial
+                ;     push -1                           ; indica que não usou
+                ;     add ecx, DWORD [ebp+ebx*4]
+                ;     dec ecx 1
+                ;     push ecx                          ; push ultimo endereço do bloco
+
+
+                ; call f2
+                ; mov ecx, DWORD quant_blocks           ; ecx = q
+                ; add ecx, ecx                          ; ecx = 2q
+                ; add ecx, ecx                          ; ecx = 4q
+                ; mov edx, ecx                          ; edx = 4q
+                ; add ecx, ecx                          ; ecx = 8q
+                ; add ecx, edx                          ; ecx = 12q
+                ; add esp, ecx                          ; add esp 12 * quant_blocks
+
+                add esp, 8
                 
                 pop ebx
                 pop ecx
-
                 leave
                 ret
